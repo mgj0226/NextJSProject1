0f02dde3f96527b4f4728f739a7f177b069690ca
Michael <143526295+mgj0226@users.noreply.github.com>
2025-07-03 13:17:57 +0100

    changed the title of the website

--- nextjsproject1/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>NextJS Project 1</title>
-      <meta name="description" content="NextJS Project 1 Description" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  );
-}
