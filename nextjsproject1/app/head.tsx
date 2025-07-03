4c99e9a0158e748cf4c0d070ecd4a90b676c1c8a
Michael <143526295+mgj0226@users.noreply.github.com>
2025-07-03 13:14:01 +0100

    Create head.tsx

---
+++ nextjsproject1/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>NextJS Project 1</title>
+      <meta name="description" content="NextJS Project 1 Description" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
